1158a6c482101137b65e8bcbfcc0916f561e13c4
cathyinc <mysterious7676@gmail.com>
2026-04-11 21:49:58 +0530

    Update prog3.py

--- src/nlttk/prog3.py
+++ src/nlttk/prog3.py
@@ -1,22 +1,9 @@
 def display3():
-    code = '''import pandas as pd
-df= pd.read_excel(r"Z:\\MMD Programs\\prg3.xlsx")
-def mapper(data):
-    mapped_data =[]
-    for age in data['Age']:
-        if not pd.isna(age):
-            mapped_data.append((age,1))
-    return mapped_data
-def reducer(mapped_data):
-    total_age =0
-    count =0
-    for age, c in mapped_data:
-        total_age += age
-        count += c
-    return total_age/count
+    code = '''name = input("Enter your name: ")
+age = int(input("Enter your age: "))
 
-mapped = mapper(df)
-average_age=reducer(mapped)
-
-print("Average Age of Titanic Passengers:",round(average_age,2))'''
+if age >= 18:
+    print(name, "is eligible to vote.")
+else:
+    print(name, "is not eligible to vote.")'''
     print(code)
