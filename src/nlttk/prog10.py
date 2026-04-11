108d962356d902d79d18133660b9e2cdc9a61876
cathyinc <mysterious7676@gmail.com>
2026-04-11 21:48:09 +0530

    Update prog10.py

--- src/nlttk/prog10.py
+++ src/nlttk/prog10.py
@@ -1,7 +1,10 @@
-stack.append('b') 
-stack.append('c') 
-print("initial stack") 
-print(stack.pop()) 
-print(stack.pop()) 
-print(stack.pop()) 
-print("\n stack after elements are popped:") print(stack) 
+def display10():
+    code = '''stack.append('b')
+stack.append('c')
+print("initial stack")
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print("\\n stack after elements are popped:")
+print(stack)'''
+    print(code)
