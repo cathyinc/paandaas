5212a2610ab475499c5b4c54241919a35fcde36d
cathyinc <mysterious7676@gmail.com>
2026-04-12 19:44:04 +0530

    Create prog12.py

---
+++ src/nlttk/prog12.py
@@ -0,0 +1,9 @@
+def display12():
+    code = '''from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+ps=PorterStemmer()
+words=["program", "programs",
+"programmer","programming","programmers"]
+for w in words:
+     print(w, ": ", ps.stem(w))'''
+    print(code)
